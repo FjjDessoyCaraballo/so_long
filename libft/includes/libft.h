99f8b9e8a9f3f7b5d0115154856aedd9f8ad6efc
Felipe Dessoy Caraballo <63795029+FjjDessoyCaraballo@users.noreply.github.com>
2024-03-09 20:16:21 +0200

    Update libft.h

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -50,7 +50,6 @@ int		ft_toupper(int c);
 int		ft_atoi(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strjoin_mod(const char *s1, const char *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
